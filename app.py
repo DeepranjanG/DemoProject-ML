a28ad786a16ef6df56d20e4552d79d97cc093d48
DeepranjanG <kdeepu070@gmail.com>
2022-09-25 15:26:28 +0530

    exception module added

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+import sys
 from flask import Flask
 from demo.logger import logging
+from demo.exception import DemoException
 app = Flask(__name__)
 
 
 @app.route("/", methods=['GET','POST'])
 def index():
-    logging.info("We are testing logging module")
+    try:
+        raise Exception("We are testing custom exception.")
+    except Exception as e:
+        demon = DemoException(e, sys)
+        logging.info(demon.error_message)
+        logging.info("We are testing logging module")
     return "CI CD pipeline has been established."
 
 
