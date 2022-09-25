f3d1ab066c97898fb956f63125bc81dc6f9040d0
DeepranjanG <kdeepu070@gmail.com>
2022-09-25 10:32:11 +0530

    logging and constant module added

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from demo.logger import logging
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET','POST'])
+def index():
+    logging.info("We are testing logging module")
+    return "CI CD pipeline has been established."
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
